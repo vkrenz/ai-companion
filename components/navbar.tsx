10cb7c5309dae2bbefe811974cabda57f5a272c6
✧ Victor ✧ <68920741+vkrenz@users.noreply.github.com>
2023-08-30 23:35:33 -0400

    nav text created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,27 @@
 'use client'
 
+import { Menu } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+});
+
 export const Navbar = () => {
     return (
-        <div>
-            Narbar
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden" />
+                <Link href="/">
+                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
+                        companion.ai
+                    </h1>
+                </Link>
+            </div>
         </div>
     );
 }
