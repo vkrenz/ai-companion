e0f739ccfa962bd2849cef07dc7282308ab7c37d
✧ Victor ✧ <68920741+vkrenz@users.noreply.github.com>
2023-09-02 22:06:31 -0400

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { Home, Plus, Settings } from "lucide-react"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 import { cn } from "@/lib/utils"
 
 export const Sidebar = () => {
     const pathname = usePathname();
+    const router = useRouter();
 
     const routes = [
         {
@@ -29,12 +30,19 @@ export const Sidebar = () => {
         },
     ]
 
+    const onNavigate = ( url: string, pro: boolean ) => {
+        // @Todo: Check if `pro` === true
+
+        return router.push(url)
+    }
+
     return (
         <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-            <div className="p-3 flex-1 justify-center">
+            <div className="p-3 flex flex-1 justify-center">
                 <div className="space-y-2">
                     {routes.map(route => (
                         <div
+                            onClick={() => onNavigate(route.href, route.pro)}
                             key={route.href}
                             className={cn(
                                 "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
