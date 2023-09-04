a061dd986ec0a629c02b89196941288886006b61
✧ Victor ✧ <68920741+vkrenz@users.noreply.github.com>
2023-09-04 18:02:38 -0400

    `search-input` works

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,24 +1,49 @@
 "use client"
 
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react"
 import { Search } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
 
 import { Input } from "@/components/ui/input"
-import { useState } from "react"
+import { useDebounce } from "@/hooks/use-debounce"
+import queryString from "query-string"
 
 export const SearchInput = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
+    const router = useRouter()
+    const searchParams = useSearchParams()
 
-    const categoryId = searchParams.get("categoryId");
-    const name = searchParams.get("name");
+    const categoryId = searchParams.get("categoryId")
+    const name = searchParams.get("name")
 
-    const [val, setVal] = useState(name || "");
+    const [value, setValue] = useState(name || "")
+
+    // 500 = 500ms (0.5 seconds)
+    const debounceValue = useDebounce<string>(value, 500)
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = e => {
+        setValue(e.target.value)
+    }
+
+    useEffect(() => {
+        const query = {
+            name: debounceValue,
+            categoryId,
+        }
+
+        const url = queryString.stringifyUrl({
+            url: window.location.href,
+            query
+        }, { skipEmptyString: true, skipNull: true })
+
+        router.push(url)
+    }, [debounceValue, router, categoryId])
 
     return (
         <div className="relative">
             <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
             <Input 
+                onChange={onChange}
+                value={value}
                 placeholder="Search..."
                 className="pl-12 bg-primary/10"
             />
