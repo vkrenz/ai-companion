35bf32f6b1e525da63f91b8e2330ac0a417bedb0
✧ Victor ✧ <68920741+vkrenz@users.noreply.github.com>
2023-09-04 17:13:27 -0400

    `useDebounce` hook created

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,20 @@
 "use client"
 
 import { Search } from "lucide-react"
+import { useRouter, useSearchParams } from "next/navigation"
+
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 export const SearchInput = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const categoryId = searchParams.get("categoryId");
+    const name = searchParams.get("name");
+
+    const [val, setVal] = useState(name || "");
+
     return (
         <div className="relative">
             <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
